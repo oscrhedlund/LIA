8ecc32d62464d0633f0b28eb57ab1db4d675a100
Oscar <oscrhedlund@gmail.com>
2021-12-05 16:47:34 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,140 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+
+    const showNavbar = (toggleId, navId, bodyId, headerId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId),
+    bodypd = document.getElementById(bodyId),
+    headerpd = document.getElementById(headerId)
+    
+    // Validate that all variables exist
+    if(toggle && nav && bodypd && headerpd){
+    toggle.addEventListener('click', ()=>{
+    // show navbar
+    nav.classList.toggle('show')
+    // change icon
+    toggle.classList.toggle('bx-x')
+    // add padding to body
+    bodypd.classList.toggle('body-pd')
+    // add padding to header
+    headerpd.classList.toggle('body-pd')
+    })
+    }
+    }
+    
+    showNavbar('header-toggle','nav-bar','body-pd','header')
+    
+    const linkColor = document.querySelectorAll('.nav_link')
+    
+    function colorLink(){
+    if(linkColor){
+    linkColor.forEach(l=> l.classList.remove('active'))
+    this.classList.add('active')
+    }
+    }
+    linkColor.forEach(l=> l.addEventListener('click', colorLink))
+    
+});
+
+$(".imgAdd").click(function(){
+  $(this).closest(".row").find('.imgAdd').before('<div class="col-sm-2 imgUp"><div class="imagePreview"></div><label class="btn btn-primary">Upload<input type="file" class="uploadFile img" value="Upload Photo" style="width:0px;height:0px;overflow:hidden;"></label><i class="fa fa-times del"></i></div>');
+});
+$(document).on("click", "i.del" , function() {
+	$(this).parent().remove();
+});
+$(function() {
+    $(document).on("change",".uploadFile", function()
+    {
+    		var uploadFile = $(this);
+        var files = !!this.files ? this.files : [];
+        if (!files.length || !window.FileReader) return; // no file selected, or no FileReader support
+ 
+        if (/^image/.test( files[0].type)){ // only image file
+            var reader = new FileReader(); // instance of the FileReader
+            reader.readAsDataURL(files[0]); // read the local file
+ 
+            reader.onloadend = function(){ // set image data as background of div
+                //alert(uploadFile.closest(".upimage").find('.imagePreview').length);
+uploadFile.closest(".imgUp").find('.imagePreview').css("background-image", "url("+this.result+")");
+            }
+        }
+      
+    });
+});
+
+// This example adds a search box to a map, using the Google Place Autocomplete
+// feature. People can enter geographical searches. The search box will return a
+// pick list containing a mix of places and predicted search terms.
+// This example requires the Places library. Include the libraries=places
+// parameter when you first load the API. For example:
+// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+
+"use strict";
+
+function initMap() {
+  const componentForm = [
+    'location',
+    'locality',
+    'administrative_area_level_1',
+    'country',
+    'postal_code',
+  ];
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 11,
+    center: { lat: 37.4221, lng: -122.0841 },
+    mapTypeControl: false,
+    fullscreenControl: true,
+    zoomControl: true,
+    streetViewControl: true
+  });
+  const marker = new google.maps.Marker({map: map, draggable: false});
+  const autocompleteInput = document.getElementById('location');
+  const autocomplete = new google.maps.places.Autocomplete(autocompleteInput, {
+    fields: ["address_components", "geometry", "name"],
+    types: ["address"],
+  });
+  autocomplete.addListener('place_changed', function () {
+    marker.setVisible(false);
+    const place = autocomplete.getPlace();
+    if (!place.geometry) {
+      // User entered the name of a Place that was not suggested and
+      // pressed the Enter key, or the Place Details request failed.
+      window.alert('No details available for input: \'' + place.name + '\'');
+      return;
+    }
+    renderAddress(place);
+    fillInAddress(place);
+  });
+
+  function fillInAddress(place) {  // optional parameter
+    const addressNameFormat = {
+      'street_number': 'short_name',
+      'route': 'long_name',
+      'locality': 'long_name',
+      'administrative_area_level_1': 'short_name',
+      'country': 'long_name',
+      'postal_code': 'short_name',
+    };
+    const getAddressComp = function (type) {
+      for (const component of place.address_components) {
+        if (component.types[0] === type) {
+          return component[addressNameFormat[type]];
+        }
+      }
+      return '';
+    };
+    document.getElementById('location').value = getAddressComp('street_number') + ' '
+              + getAddressComp('route');
+    for (const component of componentForm) {
+      // Location field is handled separately above as it has different logic.
+      if (component !== 'location') {
+        document.getElementById(component).value = getAddressComp(component);
+      }
+    }
+  }
+
+  function renderAddress(place) {
+    map.setCenter(place.geometry.location);
+    marker.setPosition(place.geometry.location);
+    marker.setVisible(true);
+  }
+}
